0799c4baadeac414dec216f53d49555352105e88
Elvin Ismayilov <0utsawg@gmail.com>
2024-03-08 22:30:33 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //(int a, string b) z = (5, "ahmet");
+            //Console.WriteLine(z.b);
+
+
+            //bool a = default(bool);
+
+            const double pi = 3.14;
+
+            double pi = 156;
+
+            Console.WriteLine(pi);
+        }
+    }
+}
